7fd6b5d69806dee10f591fe1b21a712c17cf0b0c
Christian Magnerfelt <christian.magnerfelt@gmail.com>
2013-05-02 12:20:43 +0200

    Added parsing of input parameters.

--- smallshell.c
+++ smallshell.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
 #define MAX_INPUT_LEN 70
+#define MAX_PARAM 35
 
 int executeSync(char *command, int argc, char *args[]);
 int executeAsync(char *command, int argc, char *args[]);
 
 int checkChildrenStatus();
 
+char * g_params [MAX_PARAM];
+char g_input[MAX_INPUT_LEN];
+
 int main(int argc, char *args[])
 {
-	char input[MAX_INPUT_LEN];
-
 	/* Setup signal handler */
 
 	bool isRunning = true;
@@ -20,11 +23,23 @@ int main(int argc, char *args[])
 		/* Check for terminated child processes */
 
 		/* Get next command */
-		fgets(input, MAX_INPUT_LEN, stdin);
-		fprintf(stderr, "Input: %s", input);
+		if(fgets(g_input, MAX_INPUT_LEN, stdin) == NULL)
+			continue;
+		//fprintf(stderr, "Input: %s", input);
 		
 		/* Parse command */
-		
+		{
+			char * param = strtok(g_input, " ");
+			int count = 0;
+			while(param != NULL)
+			{
+				g_params[count] = param;
+				//printf("%s\n", g_params[count]);
+				count++;		
+				param = strtok(NULL, " ");
+			}
+			g_params[count] = (char *) NULL;
+		}
 		/* Start synchronous process */
 
 		/* Start async process */
