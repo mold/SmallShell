e6ee353a4d64d4a8d9785b18f141254e23fda18a
Christian Magnerfelt <christian.magnerfelt@gmail.com>
2013-05-02 18:51:14 +0200

    Added async function to run processes in background. Added child status check if any background process have terminated.

--- smallshell.c
+++ smallshell.c
@@ -19,68 +19,84 @@ long getCurrentTimeMillis();
 
 char * g_params [MAX_PARAM];
 char g_input[MAX_INPUT_LEN];
+int g_numParams;
+int g_numProcesses;
+
+bool parseParams();
 
 int main()
 {
 	/* Setup signal handler */
 
+	g_numProcesses = 0;		/* Set number of processes running in background to zero */
 	bool isRunning = true;
 	while(isRunning)
 	{	
 		/* Check for terminated child processes */
+		checkChildrenStatus();
 
 		/* Get next command */
 		if(fgets(g_input, MAX_INPUT_LEN, stdin) == NULL)
 			continue;
 
-		bool isAsync = false;
-		{
-			char *newLine = strchr(g_input, '\n');
-			*newLine = '\0';
+		/* Replace end-line character with null-terminator */
+		char *newLine = strchr(g_input, '\n');
+		*newLine = '\0';
 
-			char * tilda = strchr(g_input, '&');
-			if(tilda != NULL)
-			{
-				*tilda = ' ';
-				isAsync = true;
-			}
-		}
-		/* Parse command */
-		{
-			char * param = strtok(g_input, " ");
-			if(param == NULL)
-				continue;
+		/* Parse params from input */
+		if(!parseParams())
+			continue;
 
-			int count = 0;
-			while(param != NULL)
-			{
-				g_params[count] = param;
-				count++;		
-				param = strtok(NULL, " ");
-			}
-			g_params[count] = (char *) NULL;
-		}
-
+		/* Do we want to exit the shell? */
 		if(!strcmp(g_params[0], "exit"))
 		{
 			isRunning = false;
 			continue;
 		}
 
+		/* Check if there is a trailing tilda in the params, tilda == run process asynchronously */
+		bool isAsync = false;
+		if(g_numParams > 1)
+		{
+			if(!strcmp(g_params[g_numParams - 1], "&"))
+			{
+				g_params[g_numParams - 1] = (char *) NULL;
+				isAsync = true;
+			}
+		}
 		/* Start synchronous process */
 		if(!isAsync)
 		{
 			executeSync(g_params[0], g_params);
 		}
-		/* Start async process */
+		/* Start asynchrouns process */
 		else
 		{
-
+			executeAsync(g_params[0], g_params);
 		}
 	}
 	return 0;
 }
+bool parseParams()
+{
+	char * param = strtok(g_input, " ");
+	/* Check if have characters in input */
+	if(param == NULL)
+		return false;	/* No characters, parse fail */
 
+	/* Tokenize input and store ptr to string in global params */
+	int count = 0;
+	while(param != NULL)
+	{
+		g_params[count] = param;
+		count++;		
+		param = strtok(NULL, " ");
+	}
+
+	g_params[count] = (char *) NULL;
+	g_numParams = count;
+	return true;
+}
 /**
  * Executes a synchronous (foreground) process.
 */
@@ -133,6 +149,28 @@ int executeSync(char *command, char *args[])
 */
 int executeAsync(char *command, char *args[])
 {
+	/* fork */
+	pid_t pid = fork();
+
+	if(pid == 0)
+	{
+		/* child process */
+
+		/* Execute command */
+		execvp(command, args);		
+		fprintf(stderr, "Execvp (%s) failed. errno: %d\n", command, errno);
+		exit(1);
+	}
+	else if(pid == -1){
+		/* Fork failed */
+		fprintf(stderr, "Fork for %s failed. errno: %d\n", command, errno);
+		exit(1);
+	}
+	else
+	{
+		g_numProcesses++;
+	}
+
 	return 0;
 }
 
@@ -142,6 +180,23 @@ int executeAsync(char *command, char *args[])
 */
 int checkChildrenStatus()
 {
+	/* Check if any child have terminated */
+	int i;
+	for(i = 0; i < g_numProcesses; i++)
+	{
+		int status;
+		pid_t id = waitpid(-1, &status, WNOHANG);
+
+		if(id == -1)
+		{	
+			continue;
+		}
+		if(WIFEXITED(status))
+		{
+			g_numProcesses--;
+			fprintf(stdout, "Background process (PID: %d) terminated with status %d\n", id, status);
+		}
+	}
 	return 0;
 }
 
