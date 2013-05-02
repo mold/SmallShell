9072d03816e2ffe1b66f252e4b6a2501b153a3f3
Daniel Molin <dmol@kth.se>
2013-05-02 12:22:03 +0200

    Wrote executeSync (untested)

--- smallshell.c
+++ smallshell.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
+#include <errno.h>
 #include <stdbool.h>
+#include <sys/time.h>
+#include <unistd.h>
 
 #define MAX_INPUT_LEN 70
 
@@ -7,6 +10,7 @@ int executeSync(char *command, int argc, char *args[]);
 int executeAsync(char *command, int argc, char *args[]);
 
 int checkChildrenStatus();
+long getCurrentTimeMillis();
 
 int main(int argc, char *args[])
 {
@@ -21,7 +25,7 @@ int main(int argc, char *args[])
 
 		/* Get next command */
 		fgets(input, MAX_INPUT_LEN, stdin);
-		fprintf(stderr, "Input: %s", input);
+		fprintf(stderr, "Input: %s\n", input);
 		
 		/* Parse command */
 		
@@ -36,6 +40,45 @@ int main(int argc, char *args[])
 */
 int executeSync(char *command, int argc, char *args[])
 {
+	/* Save start time */
+	long startTime = getCurrentTimeMillis();
+
+	/* fork */
+	pid_t pid = fork();
+
+	if(pid == 0)
+	{
+		/* child process */
+
+		/* Execute command */
+		execvp(command, args);		
+		fprintf(stderr, "Execvp (%s) failed. errno: %d\n", command, errno);
+		exit(1);
+	}
+	else if(pid == -1){
+		/* Fork failed */
+		fprintf(stderr, "Fork for %s failed. errno: %d\n", command, errno);
+		exit(1);
+	}
+	else
+	{
+		/* Parent process */
+
+		/* wait for child to terminate */
+		int status;
+		int res = waitpid(pid, &status, 0);
+		
+		if(res == -1)
+		{	
+			fprintf(stderr, "Wait for %d failed.\n", pid);
+			exit(1);
+		}
+
+		/* print child status and execution duration */
+		long durationTimeMillis = startTime - getCurrentTimeMillis();
+		fprintf(stdout, "Stopped: %s (PID: %d) with status %d Runtime: %lo ms.\n", command, pid, status, durationTimeMillis);
+	}
+
 	return 0;
 }
 
@@ -55,3 +98,18 @@ int checkChildrenStatus()
 {
 	return 0;
 }
+
+/**
+* Returns the current time in milliseconds (since the Epoch).
+*/
+long getCurrentTimeMillis()
+{
+	struct timeval tv;
+	int res = gettimeofday(&tv, NULL);
+
+	fprintf(stderr, "sec: %d usec: %d\n", tv.tv_sec, tv.tv_usec);	
+
+	return (long)(tv.tv_sec*1000 + tv.tv_usec/1000);
+}
+
+
